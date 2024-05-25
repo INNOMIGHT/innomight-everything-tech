87c39b61e20375921cba9519fd8a8a5800405bd1
Vaibhav Shrivastava <innomightmail@gmail.com>
2024-05-25 13:32:24 +0530

    deployment

---
+++ src/Components/ComingSoon.tsx
@@ -0,0 +1,29 @@
+export const ComingSoon = () => {
+    return (
+        <div className="coming-soon">
+            <h1 className="display-4">Coming Soon</h1>
+            <p className="lead">We're working hard to bring you an amazing experience. Stay tuned!</p>
+            <p>If you require any of these products or services:</p>
+            <ul className="list-unstyled">
+            <li>Speakers & Headphones</li>
+            <li>Gaming PC Build</li>
+            <li>Sound Equipments</li>
+            <li>Office Room Setup</li>
+            <li>Smart Décor Gadgets</li>
+            <li>Gaming Room Setup</li>
+            <li>PC Components</li>
+            <li>Sound System Setup</li>
+            <li>Office PC Build</li>
+            <li>Consultation & Repair</li>
+            </ul>
+            <p>Contact us at: <br /> 9755057765, 7999459656 <br /> Or visit: <a href="https://maps.app.goo.gl/LRbPspx3YLhyYf6s5" target="_blank">Our Location</a></p>
+            <div className="social-icons">
+            <a href="https://www.instagram.com/innomight" target="_blank" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/innomight" target="_blank" aria-label="Facebook"><i className="fab fa-facebook"></i></a>
+            <a href="https://x.com/Innomight1" target="_blank" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
+            </div>
+        </div>
+
+
+    )
+}
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+import logo from '../assets/Logo.png'; // Adjust the path if necessary
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img src={logo} alt="Logo" className='brand-logo'/></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    )
+}
